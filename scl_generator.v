cb822df4fb81e12ca0e0f785db5a88c2ffbb6836
AllenPianoMan <allenn_farcas@yahoo.com>
2017-06-06 00:43:14 +0300

    Add files via upload

--- scl_generator.v
+++ scl_generator.v
@@ -2,10 +2,11 @@ module scl_generator
   (
    input      clk,
    input      rst_,
+   input      enable,
    output reg scl_out
    );
 
-   reg [5:0]  cnt;
+   reg [5:0]  cnt_ff;
    reg 	      scl_out_d;
    reg [5:0]  cnt_d;
    
@@ -14,24 +15,29 @@ module scl_generator
 	if(!rst_)
 	  begin
 	     scl_out <= 1'd1;
-	     cnt <= 6'd0;
+	     cnt_ff <= 6'd0;
 	  end
 	else
 	  begin
-	     scl_out <= scl_out_d;
-	     cnt <= cnt_d;
-	  end
-     end
+	     if(enable)
+	       begin
+		  scl_out <= scl_out_d;
+		  cnt_ff <= cnt_d;
+	       end
+	  end // else: !if(!rst_)
+     end // always @ (posedge clk, negedge rst_)
 
    always @ (*)
      begin
-	scl_out_d = scl_out;
-	cnt_d = cnt + 1;
-	if (cnt == 6'd49)
+	if(enable)
 	  begin
-	     scl_out_d = ~scl_out;
-	     cnt_d = 6'd0;	  
+	     cnt_d = cnt_ff + 1;
+	     if (cnt == 6'd49)
+	       begin
+		  scl_out_d = ~scl_out;
+		  cnt_d = 6'd0;	  
+	       end
 	  end
-     end
+     end // always @ (*)
    
-endmodule
+endmodule // scl_generator
